d2f28227184ec021172fe335a479fd98d2a0e967
reza <bisniskesiniaja.reza@gmail.com>
2024-02-12 08:51:43 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,19 +9,30 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-const db = process.env.DB;
+const db = process.env.MONGO_URI;
+
+// Middleware untuk mengizinkan CORS
+app.use(cors());
+
+// Middleware untuk mengizinkan CORS dari origin tertentu
+app.use((req, res, next) => {
+   res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+   res.header("Access-Control-Allow-Headers", "Content-Type");
+   res.header("Access-Control-Allow-Credentials", "true");
+   next();
+});
 
 app.use(express.json());
-app.use(cors());
+
+app.use("/auth", authRoutes);
+app.use("/api", eventRoutes);
 
 mongoose.set("strictQuery", false);
 mongoose.connect(db, {
    useNewUrlParser: true,
    useUnifiedTopology: true,
 });
-
-app.use("/auth", authRoutes);
-app.use("/api", eventRoutes);
 
 app.listen(port, () => {
    console.log(`Server berjalan di http://localhost:${port}`);
